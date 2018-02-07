247b45c67a27c6dacb770a54afc23b4583c3825b
Jess Moran <j-es-itza@hotmail.com>
2018-02-07 09:59:38 -0600

    subiendo autentication

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
+$(function(){
+  Twitch.init({clientId: 'jyckx3hg9w4r3vmi177y5mfmk2j2r5'}, function(error, status) {
 
+    if (status.authenticated) {
+      $('.twitch-disconnect').hide();
+
+    }else{
+      $('#login-info').hide();
+    }
+  });
+
+  var login = function()
+  {
+      Twitch.login({
+        scope: ['user_read', 'channel_read']
+      });
+
+  }
+
+
+  var logout = function()
+  {
+
+
+  }
+
+
+  $('.twitch-connect').click(function(e){
+    e.preventDefault();
+
+    login();
+  })
+
+  $('.twitch-connect').click(function(e){
+    e.preventDefault();
+
+    logout();
+  })
+
+})
