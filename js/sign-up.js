7e89a0a9607a21c4683e97f0cea8d925a54fd30f
Jess Moran <j-es-itza@hotmail.com>
2018-02-08 07:19:31 -0600

    Adding functions

--- js/sign-up.js
+++ js/sign-up.js
@@ -9,6 +9,31 @@ var $passwordInput = $('#password');
 var $btnSubmit = $('.btn-submit');
 
 
-function loadpage(){
-
+function loadPage(){
+  $passwordInput.keyup(btnActive);
+  $nameInput.keyup(saveData);
+  $emailInput.keyup(saveData);
+  $passwordInput.keyup(saveData);
 }
+
+function btnActive () {
+  if($(this).val().trim().length > 5) {
+    $btnSubmit.removeAttr("disabled");
+  } else {
+    $btnSubmit.attr("disabled" , true);
+  }
+}
+
+function saveData (){
+  localStorage.setItem('userName',$nameInput.val());
+  localStorage.setItem('userEmail',$emailInput.val());
+  localStorage.setItem('userPassword',$passwordInput.val());
+
+  var userName = localStorage.getItem('userName');
+  var userEmail = localStorage.getItem('userEmail');
+  var userPassword = localStorage.getItem('userPassword');
+
+  console.log(userName,userEmail,userPassword);
+}
+
+$(document).ready(loadPage);
