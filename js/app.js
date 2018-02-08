2670354196a9b516362f8e3b450eab1fa85c37d3
Jess Moran <j-es-itza@hotmail.com>
2018-02-07 22:45:41 -0600

    añadiendo comentarios

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-$(function(){
+/* Funcionalidad log in con twitch*/
 
+$(function(){
+/*función que trae data del usuario*/
   var getInfo = function(callback){
     Twitch.api({method: 'channel'}, function(error, channel) {
       callback(channel);
     });
 
   }
-
+/*inicializando twitch*/
   Twitch.init({clientId: 'jyckx3hg9w4r3vmi177y5mfmk2j2r5'}, function(error, status) {
       console.log(status);
-
+/*sí su estado está inicializado traeme la data del usuario y cambia de vista*/
     if (status.authenticated) {
-      window.location.href = "../views/info.html";
-      
+      window.location.href = "info.html";
+ /*función añade esta imagen ye ste texto de acuerdo a el log in*/
       getInfo(function(data){
           $('strong').text(data.display_name);
           $('#picture').attr('src',"https://static-cdn.jtvnw.net/user-default-pictures/0ecbb6c3-fecb-4016-8115-aa467b7c36ed-profile_image-300x300.jpg");
@@ -34,7 +36,7 @@ $(function(){
 
   }
 
-
+/*función sale y regresa a la pantalla principal*/
   var logout = function()
   {
     Twitch.logout(function(error) {
@@ -46,7 +48,7 @@ $(function(){
       $('#login-info').hide();
     });
   }
-
+/*se llama a los botones y se previene su funcionalida de estos por default */
   $('.twitch-connect').click(function(e){
     e.preventDefault();
 
@@ -55,7 +57,7 @@ $(function(){
 
   $('#btn-logout').click(function(e){
     e.preventDefault();
-    console.log("funciona el botòn");
+
     logout();
   })
 })
