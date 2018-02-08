9ecf959f67759dd07471ceb33222fae03a8ff1b1
Jess Moran <j-es-itza@hotmail.com>
2018-02-08 15:46:56 -0600

    nm

--- js/info.js
+++ js/info.js
@@ -1,42 +1,26 @@
+/*Método jquery para publicar comentarios uno encima de otro sin que desaparezcan */
 
-function addElement() {
-    // crea un nuevo div
-    // y añade contenido
-    var newDiv = document.createElement("div");
-    var newContent = document.createTextNode("Hola!¿Qué tal?");
-    newDiv.appendChild(newContent); //añade texto al div creado.
+$('#post').click(subirImagen);
 
-    // añade el elemento creado y su contenido al DOM
-    var currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
+function subirImagen() {
+  var $load = $('#load')[0].files[0];
+  console.log($load);1
+
+  var read = new FileReader();
+
+  read.onloadend = function (){
+    $('.caption').attr('src', read.result);
   }
 
+  if($load) {
+    read.readAsDataURL($load);
+  }else {
+    $('.caption').attr('src', " ")
+  }
+}
 
 
-    /*Método jquery para publicar comentarios uno encima de otro sin que desaparezcan */
-
-    $('#post').click(subirImagen);
-
-    function subirImagen() {
-      var $load = $('#load')[0].files[0];
-      console.log($load);1
-
-      var read = new FileReader();
-
-      read.onloadend = function (){
-        $('.caption').attr('src', read.result);
-      }
-
-      if($load) {
-        read.readAsDataURL($load);
-      }else {
-        $('.caption').attr('src', " ")
-      }
-    }
-
-
-
-    //Función de colocar imagen
+//Función de colocar imagen
 
 
 //Funcionalidad "agrega comentarios"
@@ -100,5 +84,3 @@ function paintComments (commentObj){
 
 
 $(document).ready(loadPage);
-
-
