6d1e5119e9d7af9ecc6a4378be25deb81993ea30
AdrianaDillarza <itzel.dillarza@gmail.com>
2018-02-08 09:52:09 -0600

    Subiendo maquetado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+// Splash view
+ $(document).ready(function() {
+   setTimeout(function() {
+       $(".container-1").fadeOut(1500);
+   },1000);
+});
 
+$(document).ready(function() {
+   setTimeout(function() {
+       $(".container-2").fadeIn(1500);
+   },1000);
+});
