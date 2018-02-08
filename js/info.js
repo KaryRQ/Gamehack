24ca4cb47a3c16f008fb66ef484640fb77af4101
ingridsag <starlling@hotmail.com>
2018-02-08 12:07:40 -0600

    funcion y espacio para cargar y enviar una imagen

--- js/info.js
+++ js/info.js
@@ -1,19 +1,38 @@
 function addElement() {
-    // crea un nuevo div 
-    // y añade contenido 
+    // crea un nuevo div
+    // y añade contenido
     var newDiv = document.createElement("div");
     var newContent = document.createTextNode("Hola!¿Qué tal?");
-    newDiv.appendChild(newContent); //añade texto al div creado. 
+    newDiv.appendChild(newContent); //añade texto al div creado.
 
-    // añade el elemento creado y su contenido al DOM 
+    // añade el elemento creado y su contenido al DOM
     var currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
+  }
+
 
 
     /*Método jquery para publicar comentarios uno encima de otro sin que desaparezcan */
-    $(document).ready(function () {
-        $("#benviar").click(function () {
-            $("#comments").prepend('<li>' + $("#comentario").val() + '</li>');
-            $("#comentario").val('');
-        });
-    });
+
+    $('#post').click(subirImagen);
+
+    function subirImagen() {
+      var $load = $('#load')[0].files[0];
+      console.log($load);1
+
+      var read = new FileReader();
+
+      read.onloadend = function (){
+        $('.caption').attr('src', read.result);
+      }
+
+      if($load) {
+        read.readAsDataURL($load);
+      }else {
+        $('.caption').attr('src', " ")
+      }
+    }
+
+
+
+    //Función de colocar imagen
